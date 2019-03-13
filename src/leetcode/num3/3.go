d79e40e4495400b1629c1f71d57bbd81300a23f6
xiayanji <xiayanjiji@gmail.com>
2019-03-13 09:37:43 +0800

    update

--- src/leetcode/num3/3.go
+++ src/leetcode/num3/3.go
@@ -7,6 +7,8 @@ import "fmt"
 //每一列都按照从上到下递增的顺序排序。
 //请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
 
+//思路，右上角，或者左下角开始查找，因为这个地方的数字，一边是一直增长，另一边是一直下降
+
 func action(target int, array [][]int) bool {
 	var row = len(array)
 	var lines = len(array[0])
