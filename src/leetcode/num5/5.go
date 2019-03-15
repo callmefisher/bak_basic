bf6c49e7db2991297d6343b8e6d7bba9d2271d84
xiayanji <xiayanjiji@gmail.com>
2019-03-15 17:35:07 +0800

    update

--- src/leetcode/num5/5.go
+++ src/leetcode/num5/5.go
@@ -3,7 +3,37 @@ package main
 import "fmt"
 
 // 链表
+type LinkNode struct {
+	Val interface{}
+	Next * LinkNode
+}
+
+func CreateLinkList(arr[]int) *LinkNode  {
+	var head = &LinkNode{}
+	var p = head
+	for i:= 0; i < len(arr) ; i++ {
+		var tmpNode = &LinkNode{Val:arr[i]}
+		p.Next = tmpNode
+		p = tmpNode
+	}
+	return  head
+}
+
+func PrintLinkList(h* LinkNode)  {
+	for p := h.Next; p != nil ; p = p.Next {
+		if p.Next == nil {
+			fmt.Print(p.Val, "  ")
+		} else {
+			fmt.Print(p.Val, " -> ")
+		}
+	}
+}
+
+
+//1. 反转单链表
+
 
 func main() {
-	fmt.Println("link")
+	var l1 = CreateLinkList([]int{1, 2, 3, 4})
+	PrintLinkList(l1)
 }
