6bafaa32b046e4348c6e8a4c4fb4a7245e59aaf4
xiayanji <xiayanjiji@gmail.com>
2019-03-18 10:26:53 +0800

    update

--- src/leetcode/num7/7.go
+++ src/leetcode/num7/7.go
@@ -1,7 +1,41 @@
 package main
 
+import "fmt"
+import "strings"
+
+//1 左旋字符串, 字符序列S=”abcXYZdef”,要求输出循环左移3位后的结果，即“XYZdefabc
+
+func LeftRotate(str string, shiftLen int) (string) {
+	var len = len(str)
+	if len  <= 1 {
+		return str
+	}
+	var tmpStr = str + str
+	var shitIndex = shiftLen % len
+	return tmpStr[shitIndex :len + shitIndex]
+	
+	
+}
+
+// 2右旋字符串
+func RightRotate(str string, shiftLen int) (string) {
+	var len = len(str)
+	if len  <= 1 || shiftLen == 0{
+		return str
+	}
+	var tmpStr = make([]string, len, len)
+	for i := 0; i < len; i++{
+		tmpStr[(shiftLen + i ) % len] =str[i:i+1]
+	}
+	return  strings.Join(tmpStr, "")
+}
+
+
 
 //字符串
 func main()  {
 
+	fmt.Println(LeftRotate("abcXYZdef", 10))
+	fmt.Println(RightRotate("abcXYZdef", 3))
+	
 }
